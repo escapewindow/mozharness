127ca008ed7a7750abb83bc964d25d9c7aedeebe
Aki Sasaki <asasaki@mozilla.com>
2011-04-28 12:01:27 -0700

    more locales tests
    --HG--
    branch : 0.4-devel

--- test/test_l10n_locales.py
+++ test/test_l10n_locales.py
@@ -18,6 +18,7 @@ class LocalesTest(locales.LocalesMixin, log.LogMixin, script.OSMixin,
     def __init__(self):
         super(LocalesTest, self).__init__()
         self.config = {}
+        self.log_obj = None
 
 class TestLocalesMixin(unittest.TestCase):
     def setUp(self):
@@ -30,6 +31,12 @@ class TestLocalesMixin(unittest.TestCase):
         l = LocalesTest()
         l.locales = ['a', 'b', 'c']
         self.assertEqual(l.locales, l.query_locales())
+
+    def test_query_locales_ignore_locales(self):
+        l = LocalesTest()
+        l.config['locales'] = ['a', 'b', 'c']
+        l.config['ignore_locales'] = ['a', 'c']
+        self.assertEqual(['b'], l.query_locales())
 
     def test_query_locales_config(self):
         l = LocalesTest()
@@ -44,3 +51,22 @@ class TestLocalesMixin(unittest.TestCase):
         locales = l.query_locales()
         locales.sort()
         self.assertEqual(ALL_LOCALES, locales)
+
+# Commenting out til we can hide the FATAL ?
+#    def test_query_locales_no_file(self):
+#        l = LocalesTest()
+#        l.config['base_work_dir'] = '.'
+#        l.config['work_dir'] = '.'
+#        try:
+#            l.query_locales()
+#        except SystemExit:
+#            pass # Good
+#        else:
+#            self.assertTrue(False, "query_locales with no file doesn't fatal()!")
+
+    def test_parse_locales_file(self):
+        l = LocalesTest()
+        locales = ['be', 'es-ES']
+        ignore_locales = ['ar', 'de']
+        l.config['ignore_locales'] = ignore_locales
+        self.assertEqual(locales, l.parse_locales_file('test/helper_files/locales.txt'))
