7fa855db37b6d4616c70793d81a918df262d23fd
Aki Sasaki <aki@escapewindow.com>
2011-09-20 12:18:43 -0700

    actions/volatile_configs change to allow for specifying actions to run inside config files.

--- configs/users/aki/mozilla-aurora_tegra_talos.py
+++ configs/users/aki/mozilla-aurora_tegra_talos.py
@@ -3,4 +3,5 @@ config = {
     "talos_zip": "file:///Users/asasaki/Desktop/talos_ADB.zip",
     "installer_url": "file:///Users/asasaki/Desktop/fennec-8.0a2.multi.android-arm.apk",
     "virtualenv_path": "/src/talosrunner/venv",
+    "actions": ["check-device"],
 }
