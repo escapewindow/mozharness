fc6658baeeaf284db400c908846789b187b6affa
Aki Sasaki <aki@escapewindow.com>
2011-11-03 15:32:21 -0700

    tegra-over-sut changes

--- configs/users/aki/tegra1.py
+++ configs/users/aki/tegra1.py
@@ -2,7 +2,7 @@ config = {
     "log_name": "talos",
     "talos_zip": "http://people.mozilla.org/~asasaki/talos_webserver.zip",
     "browser_url": "http://ftp.mozilla.org/pub/mozilla.org/mobile/nightly/latest-mozilla-central-android/fennec-10.0a1.multi.android-arm.apk",
-    "device_name": "tegra-029.build.mozilla.org",
+    "device_name": "tegra-029",
     "device_package_name": "org.mozilla.fennec",
     "talos_device_name": "tegra-029",
     "talos_branch": "mobile",
@@ -12,8 +12,8 @@ config = {
     "talos_config_file": "remote.config",
 #    "talos_web_server": "10.251.25.44:8000",
     "virtualenv_path": "/home/cltbld/aki/venv",
-    "device_protocol": "adb",
-#    "device_port": 5555,
+    "device_protocol": "sut",
+#    "device_port": "20701",
     "device_ip": "10.251.28.128",
     "device_type": "tegra",
     "enable_automation": True,
@@ -32,12 +32,4 @@ config = {
         "tag": "default",
         "dest": "talos/mobile_profile/extensions/bench@taras.glek",
     }],
-    "exes": {
-        "adb": "/tools/android-sdk-r13/platform-tools/adb",
-    },
-    "device_exes": {
-        "kill": "/data/busybox/kill",
-        "killall": "/data/busybox/killall",
-        "uptime": "/data/busybox/uptime",
-    },
 }
