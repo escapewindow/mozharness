4cb246e67f47f51210f7f9005f51881cd9475af8
Aki Sasaki <aki@escapewindow.com>
2011-11-04 17:54:02 -0700

    with comments

--- configs/users/aki/tablet1.py
+++ configs/users/aki/tablet1.py
@@ -1,23 +1,42 @@
 config = {
     "log_name": "talos",
+
+    # this talos_zip has the --develop webserver patch
     "talos_zip": "http://people.mozilla.org/~asasaki/talos_webserver.zip",
+
     "browser_url": "http://ftp.mozilla.org/pub/mozilla.org/mobile/nightly/latest-mozilla-aurora-android/fennec-9.0a2.multi.android-arm.apk",
     "device_name": "aki_tablet",
     "device_package_name": "org.mozilla.fennec_aurora",
     "talos_branch": "mobile",
+
+    # set graph_server to "''" to not use a graph_server
 #    "graph_server": "graphs-stage.mozilla.org",
     "graph_server": "''",
+
     "results_link": "/server/collect.cgi",
     "talos_suites": ["tpan"],
     "talos_config_file": "remote.config",
+
+    # this needs to be set to either your_IP:8000, or an existing webserver
+    # that serves talos.
     "talos_webserver": "10.251.25.44:8000",
+
+    # Set this to start a webserver automatically
     "start_python_webserver": True,
-#    "virtualenv_path": "/src/talosrunner/venv",
+
+    # adb or sut
     "device_protocol": "adb",
-#    "device_port": 5555,
+
+    # set this for adb-over-ip or sut.
     "device_ip": "10.251.28.128",
+
+    # setting this to tegra250 will add tegra-specific behavior
     "device_type": "non-tegra",
+
+    # enable_automation will run steps that may be undesirable for the
+    # average user.
     "enable_automation": True,
+
 #    "actions": ["check-device"],
 #    "no_actions": ["preclean", "pull", "download", "unpack"],
     "repos": [{
--- configs/users/aki/tegra1.py
+++ configs/users/aki/tegra1.py
@@ -1,24 +1,46 @@
 config = {
     "log_name": "talos",
+
+    # this talos_zip has the --develop webserver patch
     "talos_zip": "http://people.mozilla.org/~asasaki/talos_webserver.zip",
+
     "browser_url": "http://ftp.mozilla.org/pub/mozilla.org/mobile/nightly/latest-mozilla-aurora-android/fennec-9.0a2.multi.android-arm.apk",
     "device_name": "tegra-029",
     "device_package_name": "org.mozilla.fennec_aurora",
-#    "devicemanager_debug_level": 2,
     "talos_device_name": "tegra-029",
     "talos_branch": "mobile",
+
+    # set graph_server to a real graph server if you want to publish your
+    # results (the device needs to be in the database already or you'll
+    # get errors)
     "graph_server": "graphs-stage.mozilla.org",
+
     "results_link": "/server/collect.cgi",
     "talos_suites": ["tpan"],
     "talos_config_file": "remote.config",
+
+    # this needs to be set to either your_IP:8000, or an existing webserver
+    # that serves talos.
 #    "talos_webserver": "10.251.25.44:8000",
     "talos_webserver": "bm-remote.build.mozilla.org",
-    #"virtualenv_path": "/home/cltbld/aki/venv",
+
+    # adb or sut
     "device_protocol": "sut",
-#    "device_port": "20701",
+
+    # set this to >0 if you want devicemanager output.
+    # beware, this will send binary characters to your terminal
+#    "devicemanager_debug_level": 2,
+
+    # set this for adb-over-ip or sut.
     "device_ip": "10.250.49.16",
-    "device_type": "tegra",
+
+    # setting this to tegra250 will add tegra-specific behavior
+    "device_type": "tegra250",
+
+    # enable_automation will run steps that may be undesirable for the
+    # average user.
     "enable_automation": True,
+
 #    "actions": ["check-device"],
 #    "no_actions": ["preclean", "pull", "download", "unpack"],
     "repos": [{
