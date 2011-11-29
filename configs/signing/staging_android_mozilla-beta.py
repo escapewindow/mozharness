880d703f414358c74d7ac1408f310433c2563e66
Aki Sasaki <aki@escapewindow.com>
2011-11-29 12:04:57 -0800

    a lot to do with passphrases. I want to backport query_exe() for this.

--- configs/signing/staging_android_mozilla-beta.py
+++ configs/signing/staging_android_mozilla-beta.py
@@ -5,12 +5,20 @@ import os
 LOCALES = ["en-US", "multi"]
 VERSION = "9.0b3"
 BUILDNUM = 1
-PRODUCT = "fennec"
 TAG = "FENNEC_9_0b3_RELEASE"
+AUS_SERVER = "dev-stage01.build.mozilla.org"
+FTP_SERVER = "dev-stage01.build.mozilla.org"
+FTP_UPLOAD_BASE_DIR = "/pub/mozilla.org/mobile/%(version)s-candidates/build%(buildnum)d"
+#DOWNLOAD_BASE_URL = "http://%s%s" % (FTP_SERVER, FTP_UPLOAD_BASE_DIR)
+DOWNLOAD_BASE_URL = "http://ftp.mozilla.org/pub/mozilla.org/mobile/candidates/%(version)s-candidates/build%(buildnum)d"
+APK_BASE_NAME = "fennec-%(version)s.%(locale)s.android-arm.apk"
 
-#BASE_WORK_DIR = "%s/signing-work/%s-%s" % (os.environ['HOME'], PRODUCT, VERSION)
-BASE_WORK_DIR = "%s/signing-work/%s-%s" % (os.getcwd(), PRODUCT, VERSION)
+#BASE_WORK_DIR = "%s/signing-work/fennec-%s" % (os.environ['HOME'], VERSION)
+BASE_WORK_DIR = "%s/signing-work/fennec-%s" % (os.getcwd(), VERSION)
 WORK_DIR = "build%s" % str(BUILDNUM)
+
+JARSIGNER = "/tools/jdk6/bin/jarsigner"
+STORE_ALIAS = "nightly"
 
 config = {
     "locales": LOCALES,
@@ -18,6 +26,9 @@ config = {
     "base_work_dir": BASE_WORK_DIR,
     "work_dir": WORK_DIR,
     "tag_override": TAG,
+    "platforms": ['android'],
+    "apk_base_name": APK_BASE_NAME,
+    "download_base_url": DOWNLOAD_BASE_URL,
     "user_repo_override": "build",
     "repos": [{
         "repo": "http://hg.mozilla.org/%(user_repo_override)s/tools",
--- scripts/sign_android.py
+++ scripts/sign_android.py
@@ -46,11 +46,28 @@ import sys
 sys.path.insert(1, os.path.dirname(sys.path[0]))
 
 from copy import deepcopy
+import getpass
 
 from mozharness.base.errors import SSHErrorList
 from mozharness.base.log import DEBUG, INFO, WARNING, ERROR, CRITICAL, FATAL, IGNORE
 from mozharness.base.vcs.vcsbase import MercurialScript
 from mozharness.l10n.locales import LocalesMixin
+
+# So far this only references the ftp platform name.
+SUPPORTED_PLATFORMS = ["android", "android-xul"]
+JARSIGNER_ERROR_LIST = [{
+    "substr": "jarsigner error: java.lang.RuntimeException: keystore load: Keystore was tampered with, or password was incorrect",
+    "level": FATAL,
+    "explanation": "The store passphrase is probably incorrect!",
+},{
+    "regex": "jarsigner: key associated with .* not a private key",
+    "level": FATAL,
+    "explanation": "The key passphrase is probably incorrect!",
+},{
+    "substr": "jarsigner: unable to open jar file:",
+    "level": FATAL,
+    "explanation": "The apk is missing!",
+}]
 
 class SignAndroid(LocalesMixin, MercurialScript):
     config_options = [[
@@ -68,6 +85,14 @@ class SignAndroid(LocalesMixin, MercurialScript):
       "help": "Override the tags set for all repos"
      }
     ],[
+     ['--platform',],
+     {"action": "extend",
+      "dest": "platforms",
+      "type": "choice",
+      "choices": SUPPORTED_PLATFORMS,
+      "help": "Specify the platform(s) to sign"
+     }
+    ],[
      ['--user-repo-override',],
      {"action": "store",
       "dest": "user_repo_override",
@@ -75,25 +100,48 @@ class SignAndroid(LocalesMixin, MercurialScript):
       "help": "Override the user repo path for all repos"
      }
 # TODO unsigned url, signed url, ssh key/user/server/path,
+# --version
+# --buildnum
+# --previous-version
+# --previous-buildnum
 # previous build signed url, --ignore-locale, locales_file
 # aus key/user/server/path
 # verify aus url?
     ]]
 
     def __init__(self, require_config_file=True):
+        store_passphrase = None
+        key_passphrase = None
         super(SignAndroid, self).__init__(
             config_options=self.config_options,
             all_actions=[
+#                "passphrase",
                 "clobber",
                 "pull",
-#                "download",
+#                "download-unsigned-bits",
 #                "sign",
 #                "verify",
+#                "upload-signed-bits"
+#                "clobber-unsigned-bits",
+#                "download-previous-bits",
 #                "create-snippets",
-#                "upload",
+#                "upload-snippets",
             ],
             require_config_file=require_config_file
         )
+
+    def passphrase(self):
+        print "***** Enter store passphrase: ",
+        self.store_passphrase = getpass.getpass()
+        print "***** Enter key passphrase: ",
+        self.key_passphrase = getpass.getpass()
+
+    def verify_passphrases(self):
+        # TODO writeme
+        pass
+
+    def postflight_passphrase(self):
+        self.verify_passphrases()
 
     def pull(self):
         c = self.config
@@ -111,6 +159,15 @@ class SignAndroid(LocalesMixin, MercurialScript):
             repos = c['repos']
         self.vcs_checkout_repos(repos, parent_dir=dirs['abs_work_dir'],
                                 tag_override=c.get('tag_override'))
+
+    def download_unsigned_bits(self):
+        c = self.config
+        dirs = self.query_abs_dirs()
+
+    def preflight_sign(self):
+        if self.store_passphrase is None or self.key_passphrase is None:
+            self.passphrase()
+            self.verify_passphrases()
 
 if __name__ == '__main__':
     sign_android = SignAndroid()
