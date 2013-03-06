d283cebfbf037b652aa96870e811a05f9bcb9dbc
Aki Sasaki <aki@escapewindow.com>
2013-03-05 22:02:54 -0800

    starting

---
+++ configs/users/aki/hggit1.py
@@ -0,0 +1,8 @@
+config = {
+    "log_name": "testing",
+    "repos": [{
+        "repo": "http://hg.mozilla.org/mozilla-central",
+        "revision": "default",
+        "dest": "mozilla-central",
+    }],
+}
