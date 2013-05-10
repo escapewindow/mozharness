f5b445808da0ba5953443201301e398c8efd5be9
Aki Sasaki <aki@escapewindow.com>
2013-05-09 17:54:55 -0700

    copying from vcs_conversion branch for a cleaner diff

---
+++ configs/users/aki/beagle1.py
@@ -0,0 +1,91 @@
+import os
+
+CVS_MANIFEST = """[{
+"size": 1301484692,
+"digest": "89df462d8d20f54402caaaa4e3c10aa54902a1d7196cdf86b7790b76e62d302ade3102dc3f7da4145dd832e6938b0472370ce6a321e0b3bcf0ad050937bd0e9a",
+"algorithm": "sha512",
+"filename": "mozilla-cvs-history.tar.bz2"
+}]
+"""
+
+config = {
+    "log_name": "beagle",
+    "log_max_rotate": 10,
+    "repos": [{
+        "repo": "https://hg.mozilla.org/users/hwine_mozilla.com/repo-sync-tools",
+        "vcs": "hg",
+    }],
+    "conversion_dir": "beagle",
+    "initial_repo": {
+        "repo": "https://hg.mozilla.org/mozilla-central",
+        "revision": "default",
+        "repo_name": "mozilla-central",
+        "targets": [{
+            "target_dest": "beagle/.git",
+            "vcs": "git",
+        }],
+        "bare_checkout": True,
+        "vcs": "hg",
+        "branches": {
+            "default": "master",
+        },
+    },
+    "cvs_manifest": CVS_MANIFEST,
+    "tooltool_servers": ["http://runtime-binaries.pvt.build.mozilla.org/tooltool/"],
+    "env": {
+        "PATH": "%(PATH)s:/usr/libexec/git-core",
+    },
+    "conversion_repos": [{
+        "repo": "https://hg.mozilla.org/releases/mozilla-b2g18",
+        "revision": "default",
+        "repo_name": "mozilla-b2g18",
+        "targets": [{
+            "target_dest": "beagle/.git",
+            "vcs": "git",
+        }],
+        "bare_checkout": True,
+        "vcs": "hg",
+        "branches": {
+            "default": "b2g18",
+        },
+    }],
+
+    "exes": {
+        # bug 828140 - shut https warnings up.
+        # http://kiln.stackexchange.com/questions/2816/mercurial-certificate-warning-certificate-not-verified-web-cacerts
+        "hg": [os.path.join(os.getcwd(), 'build', 'venv', 'bin', 'hg'), '--config', 'web.cacerts=/etc/pki/tls/certs/ca-bundle.crt'],
+        "tooltool.py": [
+            os.path.join(os.getcwd(), 'build', 'venv', 'bin', 'python'),
+            os.path.join(os.getcwd(), 'mozharness', 'external_tools', 'tooltool.py'),
+        ],
+    },
+
+    "virtualenv_modules": [
+        "http://puppetagain.pub.build.mozilla.org/data/python/packages/hg-git-0.4.0-moz2.tar.gz",
+        "mercurial==2.2.1",
+    ],
+#    "find_links": ["http://puppetagain.pub.build.mozilla.org/data/python/packages/", ],
+
+    ## .ssh/config
+    #Host git.m.o
+    #    HostName git.mozilla.org
+    #    IdentityFile $HOME/.ssh/vcs-sync_rsa
+    #    User vcs-sync@mozilla.com
+    #Host hg.m.o
+    #    HostName hg.mozilla.org
+    #    IdentityFile $HOME/.ssh/vcs-sync_rsa
+    #    User vcs-sync@mozilla.com
+    #Host mirror_writer
+    #    # staging server
+    #    HostName git1.stage.dmz.scl3.mozilla.com
+    #    User gitolite
+    #Host *
+    #    # avoid disconnects on large pushes
+    #    ServerAliveInterval 300
+    #
+    # _ known_hosts will have to be prepopulated.
+
+    # Disallow sharing.  We may need a better way of doing this.
+    "vcs_share_base": None,
+    "hg_share_base": None,
+}
---
+++ scripts/poc_beagle.py
@@ -0,0 +1,325 @@
+#!/usr/bin/env python
+# ***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+# ***** END LICENSE BLOCK *****
+"""poc_beagle.py
+
+Proof of concept for multi-repo m-c hg<->gitmo conversions with cvs prepending.
+"""
+
+import os
+import sys
+
+sys.path.insert(1, os.path.dirname(sys.path[0]))
+
+from mozharness.base.errors import HgErrorList, TarErrorList
+from mozharness.base.log import FATAL
+from mozharness.base.python import VirtualenvMixin, virtualenv_config_options
+from mozharness.base.vcs.vcsbase import VCSScript
+from mozharness.mozilla.tooltool import TooltoolMixin
+
+
+# HgGitScript {{{1
+class HgGitScript(VirtualenvMixin, TooltoolMixin, VCSScript):
+
+    def __init__(self, require_config_file=True):
+        super(HgGitScript, self).__init__(
+            config_options=virtualenv_config_options,
+            all_actions=[
+                'clobber',
+                'pull',
+                'create-virtualenv',
+                'create-stage-mirror',
+                'create-work-mirror',
+                'initial-conversion',
+                'prepend-cvs',
+                'munge-mapfile',
+                'create-test-target',
+                'update-stage-mirror',
+                'update-work-mirror',
+                'verify',
+                'push',
+                'notify',
+            ],
+            default_actions=[
+                'clobber',
+                'pull',
+                'create-virtualenv',
+                'create-stage-mirror',
+                'create-work-mirror',
+                'initial-conversion',
+                'prepend-cvs',
+                'munge-mapfile',
+                'create-test-target',
+                'update-stage-mirror',
+                #'update-work-mirror',
+                #'push',
+            ],
+            require_config_file=require_config_file
+        )
+
+    # Helper methods {{{1
+    def _init_hg_repo(self, path, additional_args=None):
+        hg = self.query_exe("hg", return_type="list")
+        cmd = hg + ['init']
+        if additional_args:
+            cmd.extend(additional_args)
+        cmd.append(path)
+        return self.retry(self.run_command, args=(cmd, ), error_level=FATAL, error_message="Can't set up %s!" % path)
+
+    def query_abs_dirs(self):
+        if self.abs_dirs:
+            return self.abs_dirs
+        abs_dirs = super(HgGitScript, self).query_abs_dirs()
+        abs_dirs['abs_cvs_history_dir'] = os.path.join(abs_dirs['abs_work_dir'], 'mozilla-cvs-history')
+        abs_dirs['abs_conversion_dir'] = os.path.join(abs_dirs['abs_work_dir'], 'conversion', self.config['conversion_dir'])
+        abs_dirs['abs_source_dir'] = os.path.join(abs_dirs['abs_work_dir'], 'stage_source')
+        abs_dirs['abs_repo_sync_tools_dir'] = os.path.join(abs_dirs['abs_work_dir'], 'repo-sync-tools')
+        abs_dirs['abs_git_rewrite_dir'] = os.path.join(abs_dirs['abs_work_dir'], 'mc-git-rewrite')
+        abs_dirs['abs_target_dir'] = os.path.join(abs_dirs['abs_work_dir'], 'target')
+        self.abs_dirs = abs_dirs
+        return self.abs_dirs
+
+    def query_all_repos(self):
+        return [self.config['inital_repo']] + self.config['conversion_repos']
+
+    def _update_stage_repo(self, repo_config, retry=True, clobber=False):
+        hg = self.query_exe('hg', return_type='list')
+        dirs = self.query_abs_dirs()
+        source_dest = os.path.join(dirs['abs_source_dir'], repo_config['repo_name'])
+        if clobber:
+            self.rmtree(source_dest)
+        if not os.path.exists(source_dest):
+            if self.retry(
+                self.run_command,
+                args=(hg + ['clone', '--noupdate', repo_config['repo'], source_dest], ),
+                kwargs={
+#                   'idle_timeout': 15 * 60,
+                    'cwd': dirs['abs_work_dir'],
+                    'error_list': HgErrorList,
+                },
+            ):
+                if retry:
+                    return self._update_stage_repo(repo_config, retry=False, clobber=True)
+                else:
+                    self.fatal("Can't clone %s!" % repo_config['repo'])
+        cmd = hg + ['pull']
+        if self.retry(
+            self.run_command,
+            args=(cmd, ),
+            kwargs={
+                #'idle_timeout': 15 * 60,
+                'cwd': source_dest,
+            },
+        ):
+            if retry:
+                return self._update_stage_repo(repo_config, retry=False, clobber=True)
+            else:
+                self.fatal("Can't pull %s!" % repo_config['repo'])
+        if self.run_command(hg + ["verify"], cwd=source_dest):
+            if retry:
+                return self._update_stage_repo(repo_config, retry=False, clobber=True)
+            else:
+                self.fatal("Can't verify %s!" % source_dest)
+
+    # Actions {{{1
+    def create_stage_mirror(self):
+        self.update_stage_mirror()
+
+    def create_work_mirror(self):
+        hg = self.query_exe("hg", return_type="list")
+        git = self.query_exe("git", return_type="list")
+        dirs = self.query_abs_dirs()
+        repo_config = self.config['initial_repo']
+        work_dest = dirs['abs_conversion_dir']
+        source_dest = os.path.join(dirs['abs_source_dir'], repo_config['repo_name'])
+        if not os.path.exists(work_dest):
+            self.run_command(hg + ["init", work_dest])
+        self.run_command(hg + ["pull", source_dest],
+                         cwd=work_dest,
+                         error_list=HgErrorList)
+        # Create .git for conversion, if it doesn't exist
+        git_dir = os.path.join(work_dest, '.git')
+        if not os.path.exists(git_dir):
+            self.run_command(git + ['init'], cwd=work_dest)
+            self.run_command(git + ['--git-dir', git_dir, 'config', 'gc.auto', '0'], cwd=work_dest)
+        # Update .hg/hgrc, if not already updated
+        hgrc = os.path.join(work_dest, '.hg', 'hgrc')
+        contents = ''
+        if os.path.exists(hgrc):
+            contents = self.read_from_file(hgrc)
+        if 'hggit=' not in contents:
+            hgrc_update = """[extensions]
+hggit=
+[git]
+intree=1
+"""
+            self.write_to_file(hgrc, hgrc_update, open_mode='a')
+
+    def initial_conversion(self):
+        hg = self.query_exe("hg", return_type="list")
+        dirs = self.query_abs_dirs()
+        # TODO more error checking
+        repo_config = self.config['initial_repo']
+        source = os.path.join(dirs['abs_source_dir'], repo_config['repo_name'])
+        dest = dirs['abs_conversion_dir']
+        for (branch, target_branch) in repo_config['branches'].items():
+            output = self.get_output_from_command(hg + ['id', '-r', branch], cwd=source)
+            if output:
+                rev = output.split(' ')[0]
+            self.run_command(hg + ['pull', '-r', rev, source], cwd=dest,
+                             error_list=HgErrorList)
+            self.run_command(hg + ['bookmark', '-f', '-r', rev, target_branch], cwd=dest,
+                             error_list=HgErrorList)
+            self.retry(
+                self.run_command,
+                args=(hg + ['-v', 'gexport'], ),
+                kwargs={
+#                    'idle_timeout': 15 * 60,
+                    'cwd': dest,
+                    'error_list': HgErrorList,
+                },
+                error_level=FATAL,
+            )
+            self.copy_to_upload_dir(os.path.join(dest, '.hg', 'git-mapfile'),
+                                    dest="pre-cvs-mapfile")
+
+    def _check_initial_git_revisions(self, repo_path, expected_sha1, expected_sha2):
+        git = self.query_exe('git', return_type='list')
+        output = self.get_output_from_command(git + ['log', '--oneline', '--grep', '374866'],
+                                              cwd=repo_path)
+        # hardcode test
+        if not output:
+            self.fatal("No output from git log!")
+        rev = output.split(' ')[0]
+        if not rev.startswith(expected_sha1):
+            self.fatal("Output doesn't match expected sha %s for initial hg commit: %s" % (expected_sha1, str(output)))
+        output = self.get_output_from_command(git + ['log', '-n', '1', '%s^' % rev],
+                                              cwd=repo_path)
+        if not output:
+            self.fatal("No output from git log!")
+        rev = output.splitlines()[0].split(' ')[1]
+        if rev != expected_sha2:
+            self.fatal("Output rev %s doesn't show expected rev %s:\n\n%s" % (rev, expected_sha2, output))
+
+    def prepend_cvs(self):
+        dirs = self.query_abs_dirs()
+        git = self.query_exe('git', return_type='list')
+        conversion_dir = dirs['abs_conversion_dir']
+        grafts_file = os.path.join(conversion_dir, '.git', 'info', 'grafts')
+        if not os.path.exists(dirs["abs_cvs_history_dir"]):
+            # TODO figure something else out here
+            # gd2 doesn't have access to tooltool :(
+            #manifest_path = self.create_tooltool_manifest(self.config['cvs_manifest'])
+            #if self.tooltool_fetch(manifest_path, output_dir=dirs['abs_work_dir']):
+            #    self.fatal("Unable to download cvs history via tooltool!")
+            self.run_command(["tar", "xjvf", "mozilla-cvs-history.tar.bz2"], cwd=dirs["abs_work_dir"],
+                             error_list=TarErrorList, halt_on_failure=True)
+        # We need to git checkout, or git thinks we've removed all the files
+        # without committing
+        self.run_command(git + ["checkout"], cwd=conversion_dir)
+        self.run_command('ln -s ' + os.path.join(dirs['abs_cvs_history_dir'], 'objects', 'pack', '*') +
+                         ' .', cwd=os.path.join(conversion_dir, '.git', 'objects', 'pack'))
+        self._check_initial_git_revisions(dirs['abs_cvs_history_dir'], 'e230b03',
+                                          '3ec464b55782fb94dbbb9b5784aac141f3e3ac01')
+        self._check_initial_git_revisions(conversion_dir, '4b3fd9',
+                                          '2514a423aca5d1273a842918589e44038d046a51')
+        self.write_to_file(grafts_file,
+                           '2514a423aca5d1273a842918589e44038d046a51 3ec464b55782fb94dbbb9b5784aac141f3e3ac01')
+        # This script is modified from git-filter-branch from git.
+        # https://people.mozilla.com/~hwine/tmp/vcs2vcs/notes.html#initial-conversion
+        # We may need to update this script if we update git.
+        env = self.query_env()
+        git_filter_branch = os.path.join(dirs['abs_repo_sync_tools_dir'], 'git-filter-branch-keep-rewrites')
+        self.run_command([git_filter_branch, '--', '3ec464b55782fb94dbbb9b5784aac141f3e3ac01..HEAD'],
+                         env=env, cwd=conversion_dir, halt_on_failure=True)
+        self.move(os.path.join(conversion_dir, '.git-rewrite'),
+                  dirs['abs_git_rewrite_dir'])
+        self.rmtree(grafts_file)
+
+    def munge_mapfile(self):
+        """ From https://github.com/ehsan/mozilla-history-tools/blob/master/initial_conversion/translate_git-mapfile.py
+            """
+        self.info("Updating pre-cvs mapfile...")
+        dirs = self.query_abs_dirs()
+        orig_mapfile = os.path.join(dirs['abs_work_dir'], 'pre-cvs-mapfile')
+        conversion_dir = dirs['abs_conversion_dir']
+        mapfile = os.path.join(dirs['abs_work_dir'], 'post-cvs-mapfile')
+        mapdir = os.path.join(dirs['abs_git_rewrite_dir'], 'map')
+        orig_mapfile_fh = open(orig_mapfile, "r")
+        mapfile_fh = open(mapfile, "w")
+        for line in orig_mapfile_fh:
+            tokens = line.split(" ")
+            if len(tokens) == 2:
+                git_sha = tokens[0].strip()
+                hg_sha = tokens[1].strip()
+                new_path = os.path.join(mapdir, git_sha)
+                if os.path.exists(new_path):
+                    translated_git_sha = open(new_path).read().strip()
+                    print >>mapfile_fh, "%s %s" % (translated_git_sha, hg_sha)
+                else:
+                    print >>mapfile_fh, "%s %s" % (git_sha, hg_sha)
+        orig_mapfile_fh.close()
+        mapfile_fh.close()
+        self.copyfile(mapfile, os.path.join(conversion_dir, '.hg', 'git-mapfile'))
+        self.copy_to_upload_dir(mapfile, dest="post-cvs-mapfile")
+
+    def create_test_target(self):
+        # TODO get working with query_abs_dirs
+        dirs = self.query_abs_dirs()
+        for repo_config in self.config['repos']:
+            for target_config in repo_config['targets']:
+                target_dest = os.path.join(dirs['abs_target_dir'], target_config['target_dest'])
+                if not os.path.exists(target_dest):
+                    self.info("Creating local target repo %s." % target_dest)
+                    if target_config.get("vcs", "git") == "git":
+                        self.init_git_repo(target_dest, additional_args=['--bare'])
+                    else:
+                        self.fatal("Don't know how to deal with vcs %s!" % target_config['vcs'])
+                        # TODO hg
+                else:
+                    self.debug("%s exists; skipping." % target_dest)
+
+    def update_stage_mirror(self):
+        for repo_config in self.query_all_repos():
+            self._update_stage_repo(repo_config)
+
+    def update_work_mirror(self):
+        hg = self.query_exe("hg", return_type="list")
+        dirs = self.query_abs_dirs()
+        dest = dirs['abs_conversion_dir']
+        for repo_config in self.config['repos']:
+            source = os.path.join(dirs['abs_source_dir'], repo_config['repo_name'])
+            for (branch, target_branch) in repo_config['branches'].items():
+                output = self.get_output_from_command(hg + ['id', '-r', branch], cwd=source)
+                if output:
+                    rev = output.split(' ')[0]
+                self.run_command(hg + ['pull', '-r', rev, source], cwd=dest)
+                self.run_command(hg + ['bookmark', '-f', '-r', rev, target_branch], cwd=dest)
+        self.retry(
+            self.run_command,
+            args=(hg + ['-v', 'gexport'], ),
+            kwargs={
+#                'idle_timeout': 15 * 60,
+                'cwd': dest,
+                'error_list': HgErrorList,
+            },
+            error_level=FATAL,
+        )
+        self.copy_to_upload_dir(os.path.join(dest, '.hg', 'git-mapfile'),
+                                dest="gecko-mapfile")
+
+    def push(self):
+        git = self.query_exe('git', return_type='list')
+        for repo_config in self.config['repos']:
+            work_dest = self.query_repo_dest(repo_config, 'work_dest')
+            target_dest = self.query_repo_dest(repo_config, 'target_dest')
+            self.run_command(git + ['push', '--force', '--mirror', target_dest],
+                             cwd=work_dest)
+
+# __main__ {{{1
+if __name__ == '__main__':
+    conversion = HgGitScript()
+    conversion.run()
