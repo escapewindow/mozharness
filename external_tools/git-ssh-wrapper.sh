d143011f6a677bc701ad65d146c35f4432104080
Aki Sasaki <aki@escapewindow.com>
2013-05-16 21:50:36 -0700

    fix fix?

--- external_tools/git-ssh-wrapper.sh
+++ external_tools/git-ssh-wrapper.sh
@@ -1,9 +1,7 @@
 #!/bin/sh -x
 # From http://www.reddit.com/r/git/comments/hdn1a/howto_using_the_git_ssh_variable_for_private_keys/
 
-p=`echo $GIT_SSH_KEY`
-
-if [ -e "$p" ]; then
+if [ "x$GIT_SSH_KEY" != "x" ]; then
     exec ssh -o IdentityFile="$GIT_SSH_KEY" "$@"
 else
     exec ssh "$@"
