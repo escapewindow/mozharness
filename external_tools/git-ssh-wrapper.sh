329b90453c39301ab6cb2362650a7613dce28dd6
Aki Sasaki <aki@escapewindow.com>
2013-05-16 21:48:06 -0700

    fix tilde expansion part X?

--- external_tools/git-ssh-wrapper.sh
+++ external_tools/git-ssh-wrapper.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -x
 # From http://www.reddit.com/r/git/comments/hdn1a/howto_using_the_git_ssh_variable_for_private_keys/
 
-if [ -e "$GIT_SSH_KEY" ]; then
-    exec ssh -oIdentityFile="$GIT_SSH_KEY" "$@"
+p=`echo "$GIT_SSH_KEY"`
+
+if [ -e "$p" ]; then
+    exec ssh -o IdentityFile="$GIT_SSH_KEY" "$@"
 else
     exec ssh "$@"
 fi
