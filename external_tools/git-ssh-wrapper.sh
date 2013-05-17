8e6881d91ba22854e83847fa305b0d27ec2083f2
Aki Sasaki <aki@escapewindow.com>
2013-05-16 21:48:48 -0700

    hm?

--- external_tools/git-ssh-wrapper.sh
+++ external_tools/git-ssh-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 # From http://www.reddit.com/r/git/comments/hdn1a/howto_using_the_git_ssh_variable_for_private_keys/
 
-p=`echo "$GIT_SSH_KEY"`
+p=`echo $GIT_SSH_KEY`
 
 if [ -e "$p" ]; then
     exec ssh -o IdentityFile="$GIT_SSH_KEY" "$@"
