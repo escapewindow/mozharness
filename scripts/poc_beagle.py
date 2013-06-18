dc456ac6f86e84d06137f3f4f2b20a9c7a2fc370
Aki Sasaki <aki@escapewindow.com>
2013-06-18 15:01:36 -0700

    oops, remove -f

--- scripts/poc_beagle.py
+++ scripts/poc_beagle.py
@@ -541,7 +541,7 @@ intree=1
                 continue
             branch = branch.strip()
             self.run_command(
-                [git_filter_branch, '-f', '--',
+                [git_filter_branch, '--',
                  '3ec464b55782fb94dbbb9b5784aac141f3e3ac01..%s' % branch],
                 partial_env=env,
                 cwd=git_conversion_dir,
