64f3762123deed31bec90d50a3b2aedeac58bfc2
Aki Sasaki <aki@escapewindow.com>
2013-09-10 14:35:31 -0700

    project branches config take 1

---
+++ configs/vcs_sync/project-branches.py
@@ -0,0 +1,149 @@
+import os
+import socket
+hostname = socket.gethostname()
+
+CVS_MANIFEST = """[{
+"size": 1301484692,
+"digest": "89df462d8d20f54402caaaa4e3c10aa54902a1d7196cdf86b7790b76e62d302ade3102dc3f7da4145dd832e6938b0472370ce6a321e0b3bcf0ad050937bd0e9a",
+"algorithm": "sha512",
+"filename": "mozilla-cvs-history.tar.bz2"
+}]
+"""
+
+config = {
+    "log_name": "project-branches",
+    "log_max_rotate": 99,
+    "repos": [{
+        "repo": "https://hg.mozilla.org/users/hwine_mozilla.com/repo-sync-tools",
+        "vcs": "hg",
+    }],
+    "job_name": "project-branches",
+    "conversion_dir": "project-branches",
+    "initial_repo": {
+        "repo": "https://hg.mozilla.org/mozilla-central",
+        "revision": "default",
+        "repo_name": "mozilla-central",
+        "targets": [{
+            "target_dest": "project-branches/.git",
+            "vcs": "git",
+            "test_push": True,
+        }, {
+            "target_dest": "github-project-branches",
+            "vcs": "git",
+        }],
+        "bare_checkout": True,
+        "vcs": "hg",
+        "branch_config": {
+            "branches": {
+                "default": "master",
+            },
+        },
+    },
+    "backup_dir": "/mnt/netapp/github_sync/aki/%s" % hostname,
+    "cvs_manifest": CVS_MANIFEST,
+    "tooltool_servers": ["http://runtime-binaries.pvt.build.mozilla.org/tooltool/"],
+    "cvs_history_tarball": "/home/asasaki/mozilla-cvs-history.tar.bz2",
+    "env": {
+        "PATH": "%(PATH)s:/usr/libexec/git-core",
+    },
+    "conversion_repos": [{
+        "repo": "https://hg.mozilla.org/users/asasaki_mozilla.com/dogwood",
+        "revision": "default",
+        "repo_name": "dogwood",
+        "targets": [{
+            "target_dest": "project-branches/.git",
+            "vcs": "git",
+            "test_push": True,
+        }, {
+            "target_dest": "github-project-branches",
+            "vcs": "git",
+        }],
+        "bare_checkout": True,
+        "vcs": "hg",
+        "branch_config": {
+            "branches": {
+                "default": "dogwood",
+            },
+        },
+        "tag_config": {
+        },
+    }, {
+        "repo": "https://hg.mozilla.org/projects/cedar",
+        "revision": "default",
+        "repo_name": "cedar",
+        "targets": [{
+            "target_dest": "project-branches/.git",
+            "vcs": "git",
+            "test_push": True,
+        }, {
+            "target_dest": "github-project-branches",
+            "vcs": "git",
+        }],
+        "bare_checkout": True,
+        "vcs": "hg",
+        "branch_config": {
+            "branches": {
+                "default": "cedar",
+            },
+        },
+        "tag_config": {
+        },
+    }],
+    "remote_targets": {
+        "github-project-branches": {
+            "repo": "git@github.com:escapewindow/test-project-branches.git",
+            "ssh_key": "~/.ssh/github1_rsa",
+            "vcs": "git",
+        },
+    },
+
+    "exes": {
+        # bug 828140 - shut https warnings up.
+        # http://kiln.stackexchange.com/questions/2816/mercurial-certificate-warning-certificate-not-verified-web-cacerts
+        "hg": [os.path.join(os.getcwd(), "build", "venv", "bin", "hg"), "--config", "web.cacerts=/etc/pki/tls/certs/ca-bundle.crt"],
+        "tooltool.py": [
+            os.path.join(os.getcwd(), "build", "venv", "bin", "python"),
+            os.path.join(os.getcwd(), "mozharness", "external_tools", "tooltool.py"),
+        ],
+    },
+
+    "virtualenv_modules": [
+        "bottle==0.11.6",
+        "dulwich==0.9.0",
+        "ordereddict==1.1",
+        "hg-git==0.4.0-moz2",
+        "mapper==0.1",
+        "mercurial==2.6.3",
+        "mozfile==0.9",
+        "mozinfo==0.5",
+        "mozprocess==0.11",
+    ],
+    "find_links": [
+        "http://puppetagain.pub.build.mozilla.org/data/python/packages/",
+        "http://releng-puppet2.srv.releng.use1.mozilla.com/python/packages/",
+        "http://releng-puppet1.srv.releng.use1.mozilla.com/python/packages/",
+        "http://releng-puppet2.build.mtv1.mozilla.com/python/packages/",
+        "http://releng-puppet2.srv.releng.usw2.mozilla.com/python/packages/",
+        "http://releng-puppet1.srv.releng.usw2.mozilla.com/python/packages/",
+        "http://releng-puppet2.srv.releng.scl3.mozilla.com/python/packages/",
+        "http://releng-puppet2.build.scl1.mozilla.com/python/packages/",
+    ],
+    "pip_index": False,
+
+    "upload_config": [{
+        "ssh_key": "~/.ssh/id_rsa",
+        "ssh_user": "asasaki",
+        "remote_host": "github-sync3",
+        "remote_path": "/home/asasaki/project-branches/project-branches-upload",
+    }],
+
+    "default_notify_from": "vcs2vcs@%s" % hostname,
+    "notify_config": [{
+        "to": "aki@mozilla.com",
+        "failure_only": False,
+    }],
+
+    # Disallow sharing.  We may need a better way of doing this.
+    "vcs_share_base": None,
+    "hg_share_base": None,
+}
