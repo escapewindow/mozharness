8abd0df298ecb68bb40bf58ed5541b42975b7e3f
Aki Sasaki <aki@escapewindow.com>
2013-10-06 09:32:30 -0700

    aurora -> 1.2

--- configs/vcs_sync/gecko-git.py
+++ configs/vcs_sync/gecko-git.py
@@ -47,6 +47,30 @@ config = {
         "PATH": "%(PATH)s:/usr/libexec/git-core",
     },
     "conversion_repos": [{
+        "repo": "https://hg.mozilla.org/releases/mozilla-aurora",
+        "revision": "default",
+        "repo_name": "mozilla-aurora",
+        "targets": [{
+            "target_dest": "gecko-git/.git",
+            "vcs": "git",
+            "test_push": True,
+        }, {
+            "target_dest": "github-gecko-git",
+            "vcs": "git",
+        }],
+        "bare_checkout": True,
+        "vcs": "hg",
+        "branch_config": {
+            "branches": {
+                "default": "v1.2",
+            },
+        },
+        "tag_config": {
+            "tag_regexes": [
+                "^B2G_",
+            ],
+        },
+    }, {
         "repo": "https://hg.mozilla.org/releases/mozilla-b2g18",
         "revision": "default",
         "repo_name": "mozilla-b2g18",
